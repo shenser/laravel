8da8370858fac06cda597e697e72e3f0235713d3
shenser <1399211692@qq.com>
2019-03-06 11:08:19 +0800

    3

--- public/index.php
+++ public/index.php
@@ -48,9 +48,9 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | and wonderful application we have prepared for them.
 |
 */
-echo 2;exit;
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+echo 3;exit;
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
